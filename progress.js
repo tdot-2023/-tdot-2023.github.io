05f973dec0121a577fe605f6323de0e630e56a10
mholzmann <43293295+mholzmann@users.noreply.github.com>
2023-11-21 09:10:23 +0100

    Add files via upload

---
+++ progress.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('input[type="checkbox"]').forEach(function(checkbox) {
+    checkbox.addEventListener('change', updateProgressBar);
+  });
+
+function updateProgressBar() {
+    // Get all checkboxes on the page
+    let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    
+    // Count the total number of checkboxes and checked checkboxes
+    let totalCheckboxes = checkboxes.length;
+    let checkedCount = 0;
+
+    // Loop through each checkbox and check if it is checked
+    checkboxes.forEach(function(checkbox) {
+      if (checkbox.checked) {
+        checkedCount++;
+      }
+    });
+
+    // Calculate the percentage of checked checkboxes
+    let percentage = (checkedCount / totalCheckboxes) * 100;
+
+    // Update the value attribute of the progress element
+    document.querySelector('progress').value = percentage;
+  }
