dc841f9c4d78b12736dd45ab333c972741612a88
mholzmann <m.holzmann@htl-leonding.ac.at>
2023-11-22 17:44:40 +0100

    simplfy html

--- progress.js
+++ progress.js
@@ -1,25 +1,28 @@
-document.querySelectorAll('input[type="checkbox"]').forEach(function(checkbox) {
-    checkbox.addEventListener('change', updateProgressBar);
-  });
+document.querySelectorAll('input[type="checkbox"]').forEach(function (checkbox) {
+  checkbox.addEventListener('change', updateProgressBar);
+});
+
+document.querySelector('progress').max = 100;
+document.querySelector('progress').value = 0;
 
 function updateProgressBar() {
-    // Get all checkboxes on the page
-    let checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    
-    // Count the total number of checkboxes and checked checkboxes
-    let totalCheckboxes = checkboxes.length;
-    let checkedCount = 0;
+  // Get all checkboxes on the page
+  let checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
-    // Loop through each checkbox and check if it is checked
-    checkboxes.forEach(function(checkbox) {
-      if (checkbox.checked) {
-        checkedCount++;
-      }
-    });
+  // Count the total number of checkboxes and checked checkboxes
+  let totalCheckboxes = checkboxes.length;
+  let checkedCount = 0;
 
-    // Calculate the percentage of checked checkboxes
-    let percentage = (checkedCount / totalCheckboxes) * 100;
+  // Loop through each checkbox and check if it is checked
+  checkboxes.forEach(function (checkbox) {
+    if (checkbox.checked) {
+      checkedCount++;
+    }
+  });
 
-    // Update the value attribute of the progress element
-    document.querySelector('progress').value = percentage;
-  }
+  // Calculate the percentage of checked checkboxes
+  let percentage = (checkedCount / totalCheckboxes) * 100;
+
+  // Update the value attribute of the progress element
+  document.querySelector('progress').value = percentage;
+}
